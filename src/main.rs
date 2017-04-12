97aa27b4b54add82e5e72126f703f4287ea6037e
Lorenzo Iannuzzi <lorenzoiannuzzi@gmail.com>
2017-04-12 14:29:27 +0200

    Made templates and added URL variables

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ use std::collections::HashMap;
 fn main() {
     let mut server = Nickel::new();
     server.get("/", middleware! {|_, response|
-                                 let mut a = HashMap::new();
-                                 let acdc = "Hello Lorenzo";
-                                 a.insert("ab", acdc.len());
-                                 return response.render("assets/index.tpl", &a);
+                                let mut a = HashMap::new();
+                                let acdc = "Hello Lorenzo";
+                                a.insert("ab", acdc.len());
+                                return response.render("assets/index.tpl", &a);
     });
-
+    server.get("/:name", middleware! {|request, response|
+                                      let mut b = HashMap::new();
+                                      let bcbc = request.param("name").unwrap();
+                                      if bcbc == "lorenzo" {
+                                          b.insert("name", "Creator");
+                                      } else {
+                                          b.insert("name", bcbc);
+                                      }
+                                      return response.render("assets/lang.tpl", &b);
+    });
     server.listen("127.0.0.1:6767");
 }
